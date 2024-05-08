ddd9dfc2ea9f9d772325d1335e03323a6d40d8f4
Cory Sakti <70199951+corysakti@users.noreply.github.com>
2024-05-08 21:28:09 +0700

    fix handler

--- config/router.go
+++ config/router.go
@@ -1,19 +1,35 @@
 package config
 
 import (
+	"fmt"
 	"github.com/corysakti/cats-social-go/controller"
 	"github.com/corysakti/cats-social-go/exception"
 	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
-func NewRouter(categoryController controller.CategoryController) *httprouter.Router {
+func NewRouter(handlers ...interface{}) *httprouter.Router {
 	router := httprouter.New()
 
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+	for _, handler := range handlers {
+		switch h := handler.(type) {
+		case controller.CategoryController:
+			// Add routes for the category controller
+			router.GET("/api/categories", h.FindAll)
+			router.GET("/api/categories/:categoryId", h.FindById)
+			router.POST("/api/categories", h.Create)
+			router.PUT("/api/categories/:categoryId", h.Update)
+			router.DELETE("/api/categories/:categoryId", h.Delete)
+		case func(http.ResponseWriter, *http.Request):
+			// Add custom routes for each additional handler
+			router.HandlerFunc("GET", "/api/customroute1", h)
+			// Add more custom routes as needed
+		// Add more cases for different types of handlers
+		default:
+			// Handle unsupported handler types
+			panic(fmt.Sprintf("Unsupported handler type: %T", h))
+		}
+	}
 
 	router.PanicHandler = exception.ErrorHandler
 
